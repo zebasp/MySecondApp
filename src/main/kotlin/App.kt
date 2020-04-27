259b06883ebb0feaf4cae1875f7f73b5a54c9ce2
zebasp <pazseba@hotmail.com>
2020-04-26 22:41:05 -0300

    topic-1 - Code Magnets

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,10 @@
 fun main(args:Array<String>){
-    val x =3
-    val y = 1
-    println(if(x > y) "x is greater than y" else "x is not greater than y")
-    println("This line runs no matter what")
+    var x = 1
+
+    while( x < 3){
+        print(if(x == 1) "Yab" else "Dab")
+        print("ba")
+        x = x + 1
+    }
+    if( x == 3) println("Do")
 }
