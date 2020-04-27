9e9b55d3fa75682e3940321ed66c7259225205b3
zebasp <pazseba@hotmail.com>
2020-04-26 22:12:59 -0300

    if returns value

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,6 @@
 fun main(args:Array<String>){
-    println("Hello!")
+    val x =3
+    val y = 1
+    println(if(x > y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what")
 }
