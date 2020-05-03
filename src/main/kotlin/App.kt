0f22af6704f6b22ce0bcb8ed279cd540c9e2bcb9
zebasp <pazseba@hotmail.com>
2020-05-03 19:07:47 -0300

    topic-2 - using arrayOf

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,9 @@
 fun main(args:Array<String>){
-    val x =3
-    val y = 1
-    println(if(x > y) "x is greater than y" else "x is not greater than y")
-    println("This line runs no matter what")
+   var myArray = arrayOf(1,2,3)
+    var i = 0
+
+    while (i<myArray.size){
+        println(myArray[i])
+        i = i + 1
+    }
 }
